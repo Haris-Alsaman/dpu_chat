6066e22a3fc7de41bf2085bbf443a567d1a606de
Alharath Alsaman <alharth.alsaman@gmail.com>
2024-03-27 03:56:58 +0300

    first commit

---
+++ android/app/src/main/kotlin/com/example/dpu_chat/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.dpu.dpu_chat
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:dpu_chat/screens/chat_screen.dart';
+import 'package:dpu_chat/screens/registration_screen.dart';
+import 'package:dpu_chat/screens/signin_screen.dart';
+import 'package:flutter/material.dart';
+
+import 'screens/welcome_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'MessageMe app',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        // home: ChatScreen(),
+        initialRoute: WelcomeScreen.screenRoute,
+        routes: {
+          WelcomeScreen.screenRoute: (context) => WelcomeScreen(),
+          SignInScreen.screenRoute: (context) => SignInScreen(),
+          RegistrationScreen.screenRoute: (context) => RegistrationScreen(),
+          ChatScreen.screenRoute: (context) => ChatScreen(),
+        });
+  }
+}
